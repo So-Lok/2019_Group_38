48464b901588d0717d22d90e6839a43802ad8136
Watcharapong Chaiyapong <eeywc2@nottingham.ac.uk>
2019-12-05 16:21:40 +0000

    start vector class

---
+++ vector/Vector.h
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <math.h>
+#include <ctime>
+#include <numeric>
+
+class myVector{
+private:
+	float x, y, z;
+
+
+
+public:
+	//myVector(std::vector<int> v);
+	myVector(float new_x, float new_y, float new_z);
+	std::vector<int> vect;
+	~myVector();
+	int rand_numx;
+	int rand_numy;
+	int rand_numz;
+	float get_x();
+	float get_y();
+	float get_z();
+	float magnitude();
+	float dot_product(const myVector& vec1,myVector& vec2);
+	float cross_product(const myVector& vec1, myVector& vec2);
+
+	
+
+
+
+
+
+
+
+
+};

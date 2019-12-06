bdfab91e70fab5d585a5d64e938b125f8313436e
So-Lok Wong <eeysw9@nottingham.ac.uk>
2019-12-06 16:43:38 +0000

    model test main updated

--- modelTest/main.cpp
+++ modelTest/main.cpp
@@ -8,5 +8,20 @@ int main()
 {
     model test;
 
+    //vectorClass testV(12.6, 5.2, 6.7);
+    //cout << testV.getX() << testV.getY() << testV.getZ();
+
+    // reads the mod file which is currently manually set by the user in the cpp code
     test.readFile();
+
+    //test.dispVectorList();
+
+
+ // test.dispMaterials();
+    int numVertices;
+    numVertices = test.getNumberOfVertices();
+    cout << numVertices;
+
+
+
 }

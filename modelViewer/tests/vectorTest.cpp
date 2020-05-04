a33522c02fd751524160b8f5298463d9f0d00b03
santibernal3 <56679923+santibernal3@users.noreply.github.com>
2020-05-04 01:37:46 +0100

    Update vectorTest.cpp

--- modelViewer/tests/vectorTest.cpp
+++ modelViewer/tests/vectorTest.cpp
@@ -59,10 +59,7 @@ int main(int argc, char *argv[])
       operatorFunctions = true;
     }
 
-
-
     if(vectorTest.magnitude() > 1.7319 && vectorTest.magnitude() < 1.7321)
-
 
     if(getSetFunctions==true &&
        operatorFunctions==true &&
